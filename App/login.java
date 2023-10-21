3f16efdb3d0dfa56272a1d155d2e5aa00f18d23f
Sara López Marín <slopezm13@eafit.edu.co>
2023-10-20 22:24:04 -0500

    Update login.java

--- App/login.java
+++ App/login.java
@@ -2,49 +2,6 @@ import java.util.Scanner;
 
 class Main {
 
-  public static void header(){
-    String clear = " ";
-    for(int i = 0; i < 100;i++){
-      clear = clear + "\n";
-    }
-    System.out.println(clear);
-    System.out.println("==========================");
-    System.out.println("           LIMS");
-    System.out.println("==========================\n");
-  }
-
-  public static int menu(){
-    Scanner kb = new Scanner(System.in);
-    int opcion;
-    System.out.println("           Menú\n");
-    System.out.println("1. Inicio");
-    System.out.println("2. Escanea");
-    System.out.println("3. Progreso");
-    System.out.println("4. Perfil");
-    System.out.println("5. Cerra sesión");
-    System.out.println("0. Salir \n");
-    System.out.print("Ingresar la opción: ");
-    opcion = kb.nextInt();
-
-    if (opcion < 0 || opcion > 5) {
-      System.out.println("Opción inválida");
-      opcion = menu(); 
-    }
-
-    return opcion;
-  }
-
-  public static void renderProgreso(String[][] progreso, String[] usuario){
-    int numeracion = 1;
-    for(int i = 0; i < progreso.length; i++){
-      if(usuario[0].equals(progreso[i][3])){
-        System.out.println(numeracion+". "+ progreso[i][0]+ " ("+ progreso[i][2]+")");
-        System.out.println("     " + progreso[i][1]+"\n");
-        numeracion += 1;
-      }
-    }
-  }
-
   public static int inicioAplicacion(){
     Scanner kb = new Scanner(System.in);
     int opt;
